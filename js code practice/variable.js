4c3ad8aa630e1a9fb6f88017f993506d5da8ee79
Ahsan Habib <ahsanpub16b@gmail.com>
2022-01-08 09:52:17 -0600

    Variable Learning Done!!

---
+++ js code practice/variable.js
@@ -0,0 +1,32 @@
+
+
+// variable allowcation
+
+var myItem;
+myItem = 38;
+console.log(myItem);
+
+// same declearation in one line
+
+var myItem = 38;
+document.write(`my Number is = ${myItem}`);
+
+
+var a;
+let b ;
+
+a = 45;
+b = "Ahsan";
+
+console.log(a , b);
+
+
+// variable using constant
+//benifit of constant : variable value are changable but const valu non changeable
+
+const f = 2 ;
+console.log (`constant ${f}`);
+
+
+
+
