4e9b9a828564e617217205e0c8dfa873e549888a
Ahsan Habib <ahsanpub16b@gmail.com>
2022-01-26 00:24:49 +0600

    learn simple variable example from programming hero

---
+++ Jhankar-Js/variable.js
@@ -0,0 +1,9 @@
+
+//Syntax:- variable er var -> name -> value ;
+
+
+var price = 21;
+
+var temperature = 10;
+
+var age = 13;
---
+++ js code practice/operator.js
@@ -0,0 +1,7 @@
+var a = 2 ;
+let d = 3;
+
+document.write("Operator linking Ok = " + (a+d));
+
+
+// Arithmetic Operator start 
