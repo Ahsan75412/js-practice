307c95f56674ca2cad70356cc72844ccc92aa569
Ahsan Habib <ahsanpub16b@gmail.com>
2022-01-26 23:09:14 +0600

    Array index , index of learn here

---
+++ Jhankar-Js/array-index.js
@@ -0,0 +1,33 @@
+
+var bookPages = ['hablu' , 'bolod' , 'choddogosthi' , 'battary' , 'paracitamol' , 'gym'];
+
+var battaryIndex =bookPages.indexOf('battary'); // battary er position ba index dekhte indexOf();
+
+console.log('index is = ' + battaryIndex);
+
+
+
+//amra josi dekhte chai j 2 no index a k ase tahole nicher moto kore
+
+var secondIndex = bookPages[2];
+console.log('second index is = ' + secondIndex);
+
+
+
+
+
+// Ex:2 
+
+var Numbers = [45 , 46 , 88 , 90 ,20];
+
+var findIndex = Numbers.indexOf(88);
+console.log('index is = ' + findIndex);
+
+
+
+
+// kono index er valu k change korte chaile
+console.log('change korar age ' + Numbers);
+Numbers[1] = 125 ;
+console.log('change korar pore ' + Numbers);
+
