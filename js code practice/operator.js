1b5fd7815a3ef152d2c8f90bdeafe7f591a5aff9
Ahsan Habib <ahsanpub16b@gmail.com>
2022-01-26 15:34:33 +0600

    operator example exercise done here!

--- js code practice/operator.js
+++ js code practice/operator.js
@@ -5,3 +5,15 @@ document.write("Operator linking Ok = " + (a+d));
 
 
 // Arithmetic Operator start 
+
+// ruels: operand operator operand  
+//Ex: 2 + 3 
+
+
+//operator exercise:
+
+var temp = prompt("Please enter temperature: ");
+var result = 9/5*temp + 32;
+alert("Fahrenheit: " + result + " Degree");
+console.log("Task Complete!");
+
