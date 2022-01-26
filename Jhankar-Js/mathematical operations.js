aae61f741a377872c3618e368647cc23be4a9bf1
Ahsan Habib <ahsanpub16b@gmail.com>
2022-01-26 13:38:37 +0600

    Learn Some Variable Example

---
+++ Jhankar-Js/mathematical operations.js
@@ -0,0 +1,78 @@
+// add
+var onionPrice = 43;
+var eggPrice = 42;
+var eggQuantity = 7 ;
+
+var totalPrice = onionPrice + eggPrice;
+
+console.log(totalPrice);
+
+
+//subtraction
+
+var priceDiffrence = onionPrice - eggPrice;
+
+console.log(priceDiffrence);
+
+
+//multiplication
+
+var eggMultiplication = eggPrice * eggQuantity;
+
+console.log(eggMultiplication);
+
+
+
+//division
+
+var  moneyAvailable = 100;
+var orangePrice = 5 ;
+var orangeQuantity  = moneyAvailable / orangePrice ;
+
+console.log(orangeQuantity);
+
+
+
+
+// Advanced Mathematical operation shorthand start here...........................
+
+var price1 = 12 ;
+var price2 = 35 ; 
+
+var sum = price1 + price2 ;
+var sub = price1 - price2;
+var mul = price1 * price2;
+var div = price1 / price2;
+
+console.log("Total sum is  = " +  sum);
+console.log("Total sub is  = " +  sub);
+console.log("Total mul is  = " +  mul);
+console.log("Total div is  = " +  div);
+
+
+//operation shorthand variable use part:1
+
+var age = 32 ;
+age = age + 2 ; //age barbe r ager var ta change hobe 
+
+//uporer ta short kore 
+age += 22; //ans : 32 + 22 + 2
+
+
+age++; // age er man jodi shoudhu 1 baraite chai tahole variableName++;  delei hoye jabe
+console.log(age); 
+
+
+
+// shortHand variable use part:2
+
+var love  = 100;
+love  = love - 1; //full vabe
+
+love -= 1; //uporer tar short
+
+love--; // ai 3 vabei kora jay
+
+console.log(love);
+
+
--- Jhankar-Js/variable.js
+++ Jhankar-Js/variable.js
@@ -1,9 +1,22 @@
 
 //Syntax:- variable er var -> name -> value ;
 
+//Numeric variable:
 
 var price = 21;
 
 var temperature = 10;
 
 var age = 13;
+
+
+// string variable
+
+var name = "ahsan Habib";
+var address = "Dhaka";
+
+
+//Boolean variable
+
+var pass = true;
+var bariJabi = false;
