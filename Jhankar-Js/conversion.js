6586914293819f95d04f6f2699ccb57e9f2fb1c1
Ahsan Habib <ahsanpub16b@gmail.com>
2022-01-26 14:44:16 +0600

    Namming conversion and type of variable we learn

---
+++ Jhankar-Js/conversion.js
@@ -0,0 +1,41 @@
+
+
+// name conversion
+
+var firstName = 'Ahsan';
+var lastName = 'Habib';
+
+var fullName = firstName + ' ' + lastName; // ' ' ai empty string ta faka space dey
+
+console.log(fullName);
+
+
+
+var first = 'mariya';
+var last = 'Habib';
+var name = first + ' ' + last;
+
+console.log(name);
+
+
+//integer
+var sunGlass = 4;
+
+//float
+
+var price = 99.55;
+
+
+//convarting sting to number using parseInt
+
+var onionPrice = '45.2';
+var eggprice = '55.55';
+
+var onionPriceNumber = parseInt(onionPrice); // integer convert
+var eggPriceNumber = parseFloat(onionPrice); // float convert
+
+console.log(onionPriceNumber);
+console.log(eggPriceNumber);
+
+
+
---
+++ Jhankar-Js/variable-type.js
@@ -0,0 +1,30 @@
+
+// Number type variable
+
+var shirtPrice = 200;
+console.log(typeof shirtPrice);
+
+
+
+//string type variable
+
+var shirtPrice2 ='200';
+console.log(typeof shirtPrice2);
+
+
+//boolian type variable
+
+var isRainy = true ;
+console.log(typeof isRainy);
+
+
+//undefined variable
+
+var isRomantic;
+console.log(typeof isRomantic);
+
+//date-Formate
+
+var today = new Date();
+console.log(today);
+console.log( typeof today);
