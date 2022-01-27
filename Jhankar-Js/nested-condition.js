1d9deedaff340b357a58bf338e0d0ea82fa7776b
Ahsan Habib <ahsanpub16b@gmail.com>
2022-01-27 10:34:06 +0600

    nested if else learn here!!

---
+++ Jhankar-Js/nested-condition.js
@@ -0,0 +1,43 @@
+
+
+var danishPrice = 50;
+var butterBradePrice = 25;
+var toastBiscuitPrice = 10;
+var myBudget = 100;
+var packedWell = true;
+
+
+if (danishPrice < myBudget){
+    console.log('danish kheye danish jabo!');
+}
+else if (butterBradePrice < myBudget){
+    console.log('butter bon diya cha khamu!');
+}
+
+else if (toastBiscuitPrice < myBudget){
+    console.log('tost khamu!');
+}
+
+else{
+    console.log('batasa diya cha khabo!');
+}
+
+
+
+
+
+
+
+
+// 2nd example here:
+
+
+if(danishPrice < myBudget ){
+    if(packedWell == true){
+        console.log('danish khabo!');
+    }
+
+    else{
+        console.log('danish khabo na!');
+    }
+}
