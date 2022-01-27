b9f4b72cabc974a1bfe0a8f8d346fa995b4374f3
Ahsan Habib <ahsanpub16b@gmail.com>
2022-01-27 09:55:37 +0600

    simple conditional operation learn here

---
+++ Jhankar-Js/conditional.js
@@ -0,0 +1,34 @@
+
+var isFoodReady = true ;
+//jodi khabar ready hoy tahole ami khamu
+//english: if food is ready i will eat
+
+if (isFoodReady == true){
+    console.log('Mama! I will Eat Now');
+}
+
+
+var iPhonePrice = 70000;
+var myBudget =109500 ;
+
+if (iPhonePrice < myBudget){
+    console.log('Iphone diya futani marbo');
+}
+
+
+if (iPhonePrice > myBudget){
+    console.log('My Shawmi is best!');
+}
+
+
+
+
+var checkenPrice = 180 ;
+var myMoney = 50;
+
+if (checkenPrice < myMoney){
+    console.log('Yes! murgir run khabo r haddi chibabo');
+}
+else{
+    console.log('pocket a tk nai tai khamu nah');
+}
