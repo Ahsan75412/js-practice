4ef279f9f634b3ba4d6d0d81d5b6c0a7e1e8f843
Ahsan Habib <ahsanpub16b@gmail.com>
2022-01-27 10:19:23 +0600

    multiple condition learn here!!

---
+++ Jhankar-Js/multiple-conditions.js
@@ -0,0 +1,24 @@
+
+var gotJob = true ;
+var moneySaved = 250000;
+var hasFlat = true;
+
+if (gotJob == true && moneySaved > 200000 && hasFlat > true){
+    console.log('cholo biya kore feli!');
+}
+else{
+    console.log('tor kopal a biya nai !!');
+}
+
+
+
+
+
+
+
+if (gotJob == true || moneySaved > 200000){
+    console.log('cholo biya kore feli!');
+}
+else{
+    console.log('tor kopal a biya nai !!');
+}
