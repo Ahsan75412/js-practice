cc5d8f1851eb96487424836b3bd2ea2010504397
Ahsan Habib <ahsanpub16b@gmail.com>
2022-02-19 21:53:11 +0600

    learn here events and more about events

---
+++ js code practice/bohubrihi-Dom/event.js
@@ -0,0 +1,20 @@
+// Events of Js
+// https://www.w3schools.com/JS/js_events_examples.asp
+// onclick
+
+function message(){
+    console.log("Mouse over Button!!");
+}
+
+//using EventListener:-
+
+ document.getElementById('sample-button').addEventListener('click',message );
+
+ function message(){
+     console.log('button clicked!');
+ }
+
+
+
+
+ 
